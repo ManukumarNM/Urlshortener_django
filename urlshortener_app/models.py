db05790dcc64135a90e91780ededf467d5f2b938
ManukumarNM <manunm.jps@gmail.com>
2021-11-29 19:34:58 +0530

    initial commit

--- urlshortener_app/models.py
+++ urlshortener_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Urlshortener(models.Model):
+    link = models.CharField(max_length=1000)
+    #helps in generating random objects of 128 bits as ids
+    uuid = models.CharField(max_length=10)
