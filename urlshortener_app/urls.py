27ccab5883e9ad14dbdfedac51acbc40302027c9
ManukumarNM <manunm.jps@gmail.com>
2021-11-30 17:26:01 +0530

    Urls created for new view function

--- urlshortener_app/urls.py
+++ urlshortener_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create', views.create_view, name='create_view'),
+    path('<str:pk>',views.go_view, name='go_view' ),
 ]
