c099f022b08df0d598be70bd460484ba2475082d
ManukumarNM <manunm.jps@gmail.com>
2021-11-30 17:24:24 +0530

    New view functions are created

--- urlshortener_app/views.py
+++ urlshortener_app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import uuid  
+from .models import Urlshortener 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def create_view(request):
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:7]
+        new_url = Urlshortener(link=url, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+
+def go_view(request, pk):
+    url_info = Urlshortener.objects.get(uuid=pk)
+    return redirect(url_info.link)
